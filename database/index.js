e7290f13c9fcacc21551e9ab08e50b98cc0da966
Michael Eskarous <mneskarous@gmail.com>
2019-08-21 12:37:14 -0700

    Created schema

--- database/index.js
+++ database/index.js
@@ -1,4 +1,40 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/lalalime');
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('database connected');
+});
 
+let reviewSchema = new mongoose.Schema({
+  rating: Number,
+  username: String,
+  activeSinceMonth: String,
+  activeSinceDay: String,
+  activeSinceYear: String,
+  helpfulnessVotesThumbsUp: Number,
+  helpfulnessVotesThumbsDown: Number,
+  featured: Boolean,
+  location: String,
+  athleticType: String,
+  ageRange: String, 
+  bodyType: String, 
+  whatYouLike: String,
+  whatYouDidntLike: String,
+  reviewDate: Date,
+  revewTitle: String,
+  reviewBody: String,
+  wasThisReviewHelpfulYes: Number,
+  wasThisReviewHelpfulNo: Number
+});
+
+let Review = mongoose.model('Reivew', reviewSchema);
+
+let save = (data) => {
+  console.log('data',data);
+  return Review.create(data)
+}
+
+
+module.exports.save = save;
