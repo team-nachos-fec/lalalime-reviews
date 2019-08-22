129c560abcf65ec0822a429fd569e06e12655fcd
Michael Eskarous <mneskarous@gmail.com>
2019-08-22 11:49:20 -0700

    Refactored

--- server/index.js
+++ server/index.js
@@ -11,6 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.use('/', router);
+app.use('/api', router);
 
 app.listen(port, () => console.log(`app is listening on port ${port}`));
