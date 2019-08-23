df8eb3ac30fdb725773762d1e5da822a58f0d4cf
Michael Eskarous <mneskarous@gmail.com>
2019-08-22 19:22:42 -0700

    Rendered

---
+++ client/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+
+  }
+
+  render() {
+    return (
+      <div>
+        rendered
+      </div>
+    );
+  }
+
+}
+
+export default App;
