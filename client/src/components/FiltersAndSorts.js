d4a4e7c8008b1ce701778f081e449f9dfa0df592
Michael Eskarous <mneskarous@gmail.com>
2019-08-23 19:59:25 -0700

    Created styles for filter buttons

--- client/src/components/FiltersAndSorts.js
+++ client/src/components/FiltersAndSorts.js
@@ -10,33 +10,37 @@ class FiltersAndSorts extends React.Component {
 
   }
 
+  handleClick(event) {
+    
+  }
+
   render() {
     return (
       <div>
         <div className="filters-and-sorts">
-          <span className="dropdown">
-            <button onClick="myFunction()" className="dropbtn" title="See more 'rating' filters">rating</button>
-            <span id="myDropdown" className="dropdown-content">
+          <div className="filter-dropdown">
+            <button onClick="myFunction()" className="dropbtn" title="See more 'rating' filters">rating<i className="fa fa-caret-down"></i></button>
+            <div className="dropdown-content">
               <a href="#">1 star</a>
               <a href="#">2 stars</a>
               <a href="#">3 stars</a>
               <a href="#">4 stars</a>
               <a href="#">5 stars</a>
-            </span>
-          </span>
-          <span className="dropdown">
-            <button onClick="myFunction()" className="dropbtn" title="See more 'athletic type' filters">athelic type</button>
-            <span id="myDropdown" className="dropdown-content">
+            </div>
+          </div>
+          <div className="filter-dropdown">
+            <button onClick="myFunction()" className="dropbtn" title="See more 'athletic type' filters">athletic type<i className="fa fa-caret-down"></i></button>
+            <span className="dropdown-content">
               <a href="#">yogi</a>
               <a href="#">runner</a>
               <a href="#">dancer</a>
               <a href="#">cyclist</a>
               <a href="#">sweaty generalist</a>
             </span>
-          </span>
-          <span className="dropdown">
-            <button onClick="myFunction()" className="dropbtn" title="See more 'age' filters">age</button>
-            <span id="myDropdown" className="dropdown-content">
+          </div>
+          <div className="filter-dropdown">
+            <button onClick="myFunction()" className="dropbtn" title="See more 'age' filters">age<i className="fa fa-caret-down"></i></button>
+            <span className="dropdown-content">
               <a href="#">under 18</a>
               <a href="#">18-24</a>
               <a href="#">25-34</a>
@@ -46,10 +50,10 @@ class FiltersAndSorts extends React.Component {
               <a href="#">over 65</a>
               <a href="#">i prefer to keep my age on the d.l.</a>
             </span>
-          </span>
-          <span className="dropdown">
-            <button onClick="myFunction()" className="dropbtn" title="See more 'body type' filters">body type</button>
-            <span id="myDropdown" className="dropdown-content">
+          </div>
+          <div className="filter-dropdown">
+            <button onClick="myFunction()" className="dropbtn" title="See more 'body type' filters">body type<i className="fa fa-caret-down"></i></button>
+            <span className="dropdown-content">
               <a href="#">athelic</a>
               <a href="#">curvy</a>
               <a href="#">lean</a>
@@ -58,8 +62,8 @@ class FiltersAndSorts extends React.Component {
               <a href="#">slim</a>
               <a href="#">solid</a>
             </span>
-          </span>
-          <span className="dropdown">
+          </div>
+          <span className="sort-dropdown">
           <select className="dropdown">
             <option value="choose">choose a sort order</option>
             <option value="API ENDPOINT">featured reviews first</option>
