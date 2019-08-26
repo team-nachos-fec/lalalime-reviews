61a7f10e8ce477e5ee43261ee22a2e26db89d7a8
Michael Eskarous <mneskarous@gmail.com>
2019-08-26 11:29:26 -0700

    Placed page links at bottom of reviews

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -63,14 +63,14 @@ class App extends React.Component {
               </div>
             </div>
           </div>
-          {/* <div className="review-page-links">
+          <div className="review-page-links">
             <a href="#" title="1">1</a>
             <a href="#" title="2">2</a>
             <a href="#" title="3">3</a>
             <a href="#" title="4">4</a>
             <a href="#" title="5">5</a>
             <a href="#" title="next">next</a>
-          </div> */}
+          </div>
         </div>
       </div>
     );
