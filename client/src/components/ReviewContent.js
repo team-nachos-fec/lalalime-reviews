4400132e09e2f6d2232e5ff838f4a9b42fbd4dde
Michael Eskarous <mneskarous@gmail.com>
2019-08-28 14:20:27 -0700

    Fixed bug that caused popups for all reviews to show when popup for one review was wanted

--- client/src/components/ReviewContent.js
+++ client/src/components/ReviewContent.js
@@ -8,7 +8,10 @@ class ReviewContent extends React.Component {
       helpfulYes: this.props.review.wasThisReviewHelpfulYes,
       helpfulNo: this.props.review.wasThisReviewHelpfulNo,
       clickableLinkForYes: true,
-      clickableLinkForNo: true
+      clickableLinkForNo: true,
+      isReportPopupShown: false,
+      isThankYouPopupShown: false,
+      isUserPopupShown: false
     };
   
     this.handleUsernameEnter = this.handleUsernameEnter.bind(this);
@@ -22,7 +25,10 @@ class ReviewContent extends React.Component {
   
   handleUsernameEnter(event) {
     event.preventDefault();
-    $('.user-popup').fadeIn(500);
+    this.setState({
+      isUserPopupShown: !this.state.isUserPopupShown
+    })
+    // $('.user-popup').fadeIn(500);
   }
 
   handleUsernameLeave(event) {
@@ -32,21 +38,25 @@ class ReviewContent extends React.Component {
 
   handleReportLinkClick(event) {
     event.preventDefault();
-    $('#report-popup').fadeIn(500);
+    this.setState({
+      isReportPopupShown: !this.state.isReportPopupShown
+    })
+    // $('.report-popup').fadeIn(500);
   }
 
   handleReportButtonClick(event) {
     event.preventDefault();
-    $('#report-popup').fadeOut(500);
+    $('.report-popup').fadeOut(500);
   }
 
   handleHelpfulYesClick(event) {
     event.preventDefault();
     if (this.state.helpfulYes === this.props.review.wasThisReviewHelpfulYes) {
-      $('#thank-you-popup').fadeIn(500);
+      // $('#thank-you-popup').fadeIn(500);
       this.setState({
         helpfulYes: this.props.review.wasThisReviewHelpfulYes + 1,
-        clickableLinkForNo: false
+        clickableLinkForNo: false,
+        isThankYouPopupShown: !this.state.isThankYouPopupShown
       });
     } if (this.state.helpfulYes === this.props.review.wasThisReviewHelpfulYes + 1) {
       this.setState({
@@ -59,10 +69,11 @@ class ReviewContent extends React.Component {
   handleHelpfulNoClick(event) {
     event.preventDefault();
     if (this.state.helpfulNo === this.props.review.wasThisReviewHelpfulNo) {
-      $('#thank-you-popup').fadeIn(500);
+      // $('#thank-you-popup').fadeIn(500);
       this.setState({
         helpfulNo: this.props.review.wasThisReviewHelpfulNo + 1,
-        clickableLinkForYes: false
+        clickableLinkForYes: false,
+        isThankYouPopupShown: !this.state.isThankYouPopupShown
       });
     } if (this.state.helpfulNo === this.props.review.wasThisReviewHelpfulNo + 1) {
       this.setState({
@@ -88,15 +99,85 @@ class ReviewContent extends React.Component {
     let noLink;
 
     if (clickableLinkForYes === false) {
-      yesLink = <span className="footer-answer"><a href="" title="Yes" style={{ pointerEvents: 'none' }} onClick={(event) => {this.handleHelpfulYesClick(event)}}>yes &#40; {this.state.helpfulYes} &#41;</a></span>
+      yesLink = <span className="footer-answer"><a href="" title="Yes" style={{ pointerEvents: 'none' }} onClick={(event) => {this.handleHelpfulYesClick(event)}}>yes &#40; {this.state.helpfulYes} &#41;</a></span>;
     } else {
-      yesLink = <span className="footer-answer"><a href="" title="Yes" onClick={(event) => {this.handleHelpfulYesClick(event)}} >yes &#40; {this.state.helpfulYes} &#41;</a></span>
+      yesLink = <span className="footer-answer"><a href="" title="Yes" onClick={(event) => {this.handleHelpfulYesClick(event)}} >yes &#40; {this.state.helpfulYes} &#41;</a></span>;
     }
 
     if (clickableLinkForNo === false) {
-      noLink = <span className="footer-answer"><a href="" title="No" style={{ pointerEvents: 'none' }} onClick={(event) => {this.handleHelpfulNoClick(event)}}>no &#40; {this.state.helpfulNo} &#41;</a></span>
+      noLink = <span className="footer-answer"><a href="" title="No" style={{ pointerEvents: 'none' }} onClick={(event) => {this.handleHelpfulNoClick(event)}}>no &#40; {this.state.helpfulNo} &#41;</a></span>;
     } else {
-      noLink = <span className="footer-answer"><a href="" title="No" onClick={(event) => {this.handleHelpfulNoClick(event)}}>no &#40; {this.state.helpfulNo} &#41;</a></span>
+      noLink = <span className="footer-answer"><a href="" title="No" onClick={(event) => {this.handleHelpfulNoClick(event)}}>no &#40; {this.state.helpfulNo} &#41;</a></span>;
+    }
+
+    const isReportPopupShown = this.state.isReportPopupShown;
+    let reportPopup;
+
+    if (isReportPopupShown) {
+      reportPopup =             
+      <div className="report-popup">
+        <span>describe the issue</span>
+        <form>
+          <div className="report-form">
+            <textarea type="text"></textarea>
+          </div>
+          <div className="report-form-btns">
+            <input className="submit-btn" type="submit" value="Submit" 
+            onClick={(event) => {this.handleReportButtonClick(event)}} />
+            <input className="cancel-btn" type="submit" value="Cancel" 
+            onClick={(event) => {this.handleReportButtonClick(event)}} />
+          </div>
+        </form>
+      </div>;
+    } else {
+      reportPopup = '';
+    }
+
+    const isThankYouPopupShown = this.state.isThankYouPopupShown;
+    let thankYouPopup;
+
+    if (isThankYouPopupShown === true) {
+      thankYouPopup = 
+      <div id="thank-you-popup">
+        <span id="popup-text">Thank you! You have successfully submitted feedback for this review.</span>
+        <a href="" title="Close" id="popup-close-button" onClick={(event) => {this.handleHelpfulCloseClick(event)}}>&#x2715;</a>
+      </div>;
+    } else {
+      thankYouPopup = '';
+    }
+
+    const isUserPopupShown = this.state.isUserPopupShown;
+    let userPopup;
+
+    if (isUserPopupShown === true) {
+      userPopup = 
+      <div className="user-popup" onMouseLeave={(event) => {this.handleUsernameLeave(event)}}>
+        <div id="popup-username"><a href="" className="username" title={`See ${this.props.review.username}'s full profile`}>{this.props.review.username}</a></div>
+        <div id="popup-active-date">Active since: {this.props.review.activeSinceDate}</div>
+        <div id="also-reviewed-text">I also reviewed</div>
+        <div id="first-rating">
+          <span><a href="" title="Scuba Pullover">Scuba Pullover</a></span>
+          <span><img src={`https://lululemon.ugc.bazaarvoice.com/7834redes/${this.props.review.rating}_0/5/rating.gif`} alt={`${this.props.review.rating} out of 5`} title={`${this.props.review.rating} out of 5`}/></span>
+        </div>
+        <div id="rating-container">
+          <div id="second-rating">
+            <span id="text">&#40;Unknown&#41;</span>
+            <span><img src={`https://lululemon.ugc.bazaarvoice.com/7834redes/3_0/5/rating.gif`} alt={`3 out of 5`} title={`3 out of 5`}/></span>
+          </div>
+        </div>
+        <div id="helpfulness-votes">
+          <span id="helpfulness-text">Helpfulness Votes</span>
+          <span><img src={'https://i.imgur.com/GsRixgl.png'}/></span>
+          <span className="helpfulness-number">{this.props.review.helpfulnessVotesThumbsUp}</span>
+          <span><img src={'https://i.imgur.com/fWCf4UL.png'}/></span>
+          <span className="helpfulness-number">{this.props.review.helpfulnessVotesThumbsDown}</span>
+        </div>
+        <div id="full-profile-link">
+          <span><a href="" title={`See ${this.props.review.username}'s full profile`}>See full profile</a></span>
+        </div>
+      </div>;
+    } else {
+      userPopup = '';
     }
 
     return (
@@ -104,7 +185,39 @@ class ReviewContent extends React.Component {
         <div className="review-profile">
           <div className="rating"><img src={`https://lululemon.ugc.bazaarvoice.com/7834redes/${this.props.review.rating}_0/5/rating.gif`} alt={`${this.props.review.rating} out of 5`} title={`${this.props.review.rating} out of 5`}/></div>
           <a href="" className="username" onMouseEnter={(event) => {this.handleUsernameEnter(event)}}>{this.props.review.username}</a>
-          <div className="user-popup" onMouseLeave={(event) => {this.handleUsernameLeave(event)}}>
+          {userPopup}
+          <div className="user-properties">
+            <div className="user-property"><span className="lighter">location: </span><span className="bolder">{this.props.review.location.toUpperCase()}</span></div>
+            <div className="user-property"><span className="lighter">athletic type: </span><span className="bolder">{this.props.review.athleticType.toUpperCase()}</span></div>
+            <div className="user-property"><span className="lighter">age: </span><span className="bolder">{this.props.review.ageRange.toUpperCase()}</span></div>
+            <div className="user-property"><span className="lighter">body type: </span><span className="bolder">{this.props.review.bodyType.toUpperCase()}</span></div>
+          </div>
+          <div className="profile-property"><div className="bolder">what you like</div><div className="lighter">{this.props.review.whatYouLike}</div></div>
+          <div className="profile-property"><div className="bolder">what you don't like</div><div className="lighter">{this.props.review.whatYouDidntLike}</div></div>
+        </div>
+        <div className="review-content">
+          <div className="review-date">{this.props.review.reviewDate.toUpperCase()}</div>
+          <div className="review-title">{this.props.review.reviewTitle}</div>
+          <div className="review-body">{this.props.review.reviewBody}</div>
+          <div className="review-footer">
+            <span className="footer-question">Was this reivew helpful to you?</span>
+            {yesLink}
+            {noLink}
+            <span className="footer-report"><a href="" title="Report"
+            onClick={(event) => {this.handleReportLinkClick(event)}}
+            >report as inappropriate</a></span>
+            {reportPopup}
+          </div>
+          {thankYouPopup}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ReviewContent;
+
+{/* <div className="user-popup" onMouseLeave={(event) => {this.handleUsernameLeave(event)}}>
             <div id="popup-username"><a href="" className="username" title={`See ${this.props.review.username}'s full profile`}>{this.props.review.username}</a></div>
             <div id="popup-active-date">Active since: {this.props.review.activeSinceDate}</div>
             <div id="also-reviewed-text">I also reviewed</div>
@@ -128,50 +241,4 @@ class ReviewContent extends React.Component {
             <div id="full-profile-link">
               <span><a href="" title={`See ${this.props.review.username}'s full profile`}>See full profile</a></span>
             </div>
-          </div>
-          <div className="user-properties">
-            <div className="user-property"><span className="lighter">location: </span><span className="bolder">{this.props.review.location.toUpperCase()}</span></div>
-            <div className="user-property"><span className="lighter">athletic type: </span><span className="bolder">{this.props.review.athleticType.toUpperCase()}</span></div>
-            <div className="user-property"><span className="lighter">age: </span><span className="bolder">{this.props.review.ageRange.toUpperCase()}</span></div>
-            <div className="user-property"><span className="lighter">body type: </span><span className="bolder">{this.props.review.bodyType.toUpperCase()}</span></div>
-          </div>
-          <div className="profile-property"><div className="bolder">what you like</div><div className="lighter">{this.props.review.whatYouLike}</div></div>
-          <div className="profile-property"><div className="bolder">what you don't like</div><div className="lighter">{this.props.review.whatYouDidntLike}</div></div>
-        </div>
-        <div className="review-content">
-          <div className="review-date">{this.props.review.reviewDate.toUpperCase()}</div>
-          <div className="review-title">{this.props.review.reviewTitle}</div>
-          <div className="review-body">{this.props.review.reviewBody}</div>
-          <div className="review-footer">
-            <span className="footer-question">Was this reivew helpful to you?</span>
-            {yesLink}
-            {noLink}
-            <span className="footer-report"><a href="" title="Report"
-            onClick={(event) => {this.handleReportLinkClick(event)}}
-            >report as inappropriate</a></span>
-            <div id="report-popup">
-              <span>describe the issue</span>
-              <form>
-                <div className="report-form">
-                  <textarea type="text"></textarea>
-                </div>
-                <div className="report-form-btns">
-                  <input className="submit-btn" type="submit" value="Submit" 
-                  onClick={(event) => {this.handleReportButtonClick(event)}} />
-                  <input className="cancel-btn" type="submit" value="Cancel" 
-                  onClick={(event) => {this.handleReportButtonClick(event)}} />
-                </div>
-              </form>
-          </div>
-            </div>
-            <div id="thank-you-popup">
-            <span id="popup-text">Thank you! You have successfully submitted feedback for this review.</span>
-            <a href="" title="Close" id="popup-close-button" onClick={(event) => {this.handleHelpfulCloseClick(event)}}>&#x2715;</a>
-            </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-export default ReviewContent;
+          </div> */}
