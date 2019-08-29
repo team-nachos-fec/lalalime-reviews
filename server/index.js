e3fc35d0cc24d6a3e4d848ea55eba2a015d8cfbc
Michael Eskarous <mneskarous@gmail.com>
2019-08-28 19:06:31 -0700

    Changed port to 80 to remove axios error when running tests

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const router = require('./router.js');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
