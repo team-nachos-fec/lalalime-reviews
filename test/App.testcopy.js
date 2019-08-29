235f1e5e166dc4666e87d3d6015c6ae3032a9ca3
Michael Eskarous <mneskarous@gmail.com>
2019-08-28 19:39:48 -0700

    Tests have unexplainable syntax error

---
+++ test/App.testcopy.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import App from '../client/src/components/App';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App component', () => {
+  test('renders', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test('reviews are loaded onto page sorted by date', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'sortByDate');
+    instance.componentDidMount();
+    expect(instance.sortByDate).toHaveBeenCalled();
+  });
+
+  test('should render 10 reviews', () => {
+    const wrapper = shallow(<App />);
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'sortByDate');
+    instance.componentDidMount();
+    instance.setState({ reviews: reviews })
+    expect(instance.state('reviews')).toHaveLength(10);
+  });
+
+});
---
+++ test/ReviewPageLinks.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import ReviewPageLinks from '../client/src/components/ReviewPageLinks';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('ReviewPageLinks component', () => {
+  test('renders', () => {
+    const wrapper = shallow(<ReviewPageLinks />);
+      expect(wrapper.exists()).toBe(true);
+  });
+});
