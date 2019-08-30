b619f4806fdca43ea0c5c7de786390d11049561f
Michael Eskarous <mneskarous@gmail.com>
2019-08-30 10:43:23 -0700

    Unit tested MouseEnter function

--- test/ReviewContent.test.js
+++ test/ReviewContent.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import ReviewContent from '../client/src/components/ReviewContent';
 import Adapter from 'enzyme-adapter-react-16';
 
@@ -17,8 +17,34 @@ describe('ReviewContent component', () => {
       reviewDate: 'August 27, 2019'
     }
   };
+
+  const wrapper = shallow(<ReviewContent {...ReviewContentProps} />);
+
   test('renders', () => {
-    const wrapper = shallow(<ReviewContent {...ReviewContentProps} />);
-      expect(wrapper.exists()).toBe(true);
+    expect(wrapper.exists()).toBe(true);
   });
+
+  console.log('REVIEW PROFILE CHILD 1',wrapper.props('review').children[0].props.children[1].props.onMouseEnter);
+
+  test('onMouseEnter changes false to true', () => {
+    wrapper
+      .find('a.username')
+      .simulate('mouseEnter', {
+          preventDefault: () => {
+        }
+      });
+    expect(wrapper.state('isUserPopupShown')).toBe(true);
+  })
+
+
+
+
+  // console.log('REVIEW PROFILE CHILD 3',wrapper.props('review').children[0].props.children[3].props);
+  // console.log('REVIEW PROFILE CHILD 4',wrapper.props('review').children[0].props.children[4].props);
+  // console.log('REVIEW PROFILE CHILD 5',wrapper.props('review').children[0].props.children[5].props);
+
+
+  // console.log('REVIEW CONTENT',wrapper.props('review').children[1].props);
+  // console.log(wrapper.props('review').children[1].props.children[0].props);
+  // console.log(wrapper.props('review').children[1].props.children[1].props);
 });
